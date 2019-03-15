127301aa1a4d75294a0d1650ad83d9853955bd53
Lucas <lucas.keiler1984@gmail.com>
2019-03-15 20:11:49 -0300

    Capitulo PD

---
+++ competitiveProgramming3/Chapter4/UVa507.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+struct segment{
+    int begin,end,niceness;
+};
+
+int main (){
+    int b;
+    cin >> b;
+    for(int route = 1;route <= b; route++){
+        int r;
+        cin >> r;
+
+        int niceness_entries[r+1];
+        for(int i = 1;i<r;i++)
+            cin >> niceness_entries[i];
+
+        segment current, best;
+        best.begin = r;
+        best.niceness = -1;
+        current.begin = 1;
+        current.niceness = 0;
+        for(int i=1;i<r;i++){
+            if((current.niceness + niceness_entries[i]) >= 0){
+                current.niceness += niceness_entries[i];
+                current.end = i+1;
+            }else if (i < r-1){
+                current.begin = i+1;
+                current.end = i+2;
+                current.niceness = 0;
+            }
+
+            if(current.niceness > 0){
+                if(current.niceness > best.niceness)
+                    best = current;
+                else if(current.niceness == best.niceness){
+                    if(current.end - current.begin > best.end - best.begin)
+                        best = current;
+                    else if(current.end - current.begin == best.end - best.begin && current.begin < best.begin)
+                        best = current;
+
+                }
+            }
+
+        }
+        if(current.niceness > 0){
+            if(current.niceness > best.niceness)
+                best = current;
+            else if(current.niceness == best.niceness){
+                if((current.end - current.begin) > (best.end - best.begin))
+                    best = current;
+                else if(current.end - current.begin == best.end - best.begin && current.begin < best.begin)
+                    best = current;
+
+            }
+        }
+
+        if(best.niceness > 0)
+            cout << "The nicest part of route " << route << " is between stops " << best.begin << " and " << best.end;
+        else
+            cout << "Route " << route << " has no nice parts";
+        cout << endl;
+    }
+
+    return 0;
+}
